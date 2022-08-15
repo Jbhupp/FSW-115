30e36f8cfa6abb24fd45bc590104d336d69e0a77
Jbhupp <103956990+Jbhupp@users.noreply.github.com>
2022-08-14 22:01:59 -0400

    Add files via upload

---
+++ list.js
@@ -0,0 +1,76 @@
+var rangers = JSON.parse(`{
+    "eyeColor" :[
+{
+    "name": "C-3PO",
+    "height": "167",
+    "mass": "75",
+    "hair_color": "n/a",
+    "skin_color": "gold",
+    "eye_color": "yellow",
+    "birth_year": "112BBY",
+    "gender": "n/a",
+    "homeworld": "https://swapi.dev/api/planets/1/",
+    "films": [
+        "https://swapi.dev/api/films/1/",
+        "https://swapi.dev/api/films/2/",
+        "https://swapi.dev/api/films/3/",
+        "https://swapi.dev/api/films/4/",
+        "https://swapi.dev/api/films/5/",
+        "https://swapi.dev/api/films/6/"
+    ],
+}
+
+
+{
+    "name": "R2-D2",
+    "height": "96",
+    "mass": "32",
+    "hair_color": "n/a",
+    "skin_color": "white, blue",
+    "eye_color": "red",
+    "birth_year": "33BBY",
+    "gender": "n/a",
+    "homeworld": "https://swapi.dev/api/planets/8/",
+    "films": [
+        "https://swapi.dev/api/films/1/",
+        "https://swapi.dev/api/films/2/",
+        "https://swapi.dev/api/films/3/",
+        "https://swapi.dev/api/films/4/",
+        "https://swapi.dev/api/films/5/",
+        "https://swapi.dev/api/films/6/"
+    ],
+}
+
+
+{
+    "name": "Darth Vader",
+    "height": "202",
+    "mass": "136",
+    "hair_color": "none",
+    "skin_color": "white",
+    "eye_color": "yellow",
+    "birth_year": "41.9BBY",
+    "gender": "male",
+    "homeworld": "https://swapi.dev/api/planets/1/",
+    "films": [
+        "https://swapi.dev/api/films/1/",
+        "https://swapi.dev/api/films/2/",
+        "https://swapi.dev/api/films/3/",
+        "https://swapi.dev/api/films/6/"
+    ],
+    
+}
+]
+
+}`)
+
+var myEyeColor = rangers.eyeColor
+var list = document.getElementById("people");
+
+    for (i = 0; i< myEyeColor.length; i++){
+    var newName = document.createElement("li");
+    newName.textContent = myEyeColor[i].eye_color;
+    list.appendChild(newName);
+    
+}
+console.log(newName);
